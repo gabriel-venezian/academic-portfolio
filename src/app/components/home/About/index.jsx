36983774fff1098900b528959e79bef72345a076
Gabriel Venezian <gabriel.venezian@hotmail.com>
2023-10-24 11:59:32 -0300

    refactor: remove unecessary section titles

--- src/app/components/home/About/index.jsx
+++ src/app/components/home/About/index.jsx
@@ -12,39 +12,36 @@ import aboutPicture from '../../../../../public/home/about/about-picture.jpg';
 
 export default function About() {
   return (
-    <>
-      <SectionTitle id="sobre" title="Sobre" />
-      <AboutContainer>
-        <AboutContainerDecorationBg />
-        <AboutWrapper>
-          <AboutImage>
-            <Image
-              src={aboutPicture}
-              width={'auto'}
-              height={'auto'}
-              alt="Gabriel Venezian"
-            />
-          </AboutImage>
-          <AboutText>
-            <p>Olá! Sou Gabriel Venezian.</p>
-            <p>
-              Desenvolvedor Front-end apaixonado por tecnologia. Conheci
-              programação aos 11 anos, e desde então minha vida mudou.
-            </p>
-            <p>Amo jogos, livros, músicas, animes e um bom café.</p>
-            <p>
-              Trabalho com a criação e desenvolvimento de interfaces, correção
-              de erros e bugs visuais, além do aprimoramento de interfaces
-              baseado em feedbacks.
-            </p>
-            <p>
-              Estou no 6º Semestre de Sistemas para Internet, tecnólogo
-              disponibilizado pela Fatec Araras, e minha previsão de formatura é
-              dezembro de 2023.
-            </p>
-          </AboutText>
-        </AboutWrapper>
-      </AboutContainer>
-    </>
+    <AboutContainer id="sobre">
+      <AboutContainerDecorationBg />
+      <AboutWrapper>
+        <AboutImage>
+          <Image
+            src={aboutPicture}
+            width={'auto'}
+            height={'auto'}
+            alt="Gabriel Venezian"
+          />
+        </AboutImage>
+        <AboutText>
+          <p>Olá! Sou Gabriel Venezian.</p>
+          <p>
+            Desenvolvedor Front-end apaixonado por tecnologia. Conheci
+            programação aos 11 anos, e desde então minha vida mudou.
+          </p>
+          <p>Amo jogos, livros, músicas, animes e um bom café.</p>
+          <p>
+            Trabalho com a criação e desenvolvimento de interfaces, correção de
+            erros e bugs visuais, além do aprimoramento de interfaces baseado em
+            feedbacks.
+          </p>
+          <p>
+            Estou no 6º Semestre de Sistemas para Internet, tecnólogo
+            disponibilizado pela Fatec Araras, e minha previsão de formatura é
+            dezembro de 2023.
+          </p>
+        </AboutText>
+      </AboutWrapper>
+    </AboutContainer>
   );
 }
--- src/app/components/home/Experience/index.jsx
+++ src/app/components/home/Experience/index.jsx
@@ -26,48 +26,45 @@ export default function Experience() {
   const educationalInstitutions = educationalData;
 
   return (
-    <>
-      <SectionTitle id="experiencia" title="Experiência" />
-      <ExperienceContainer>
-        <ExperienceSection>
-          <ExperienceSectionTitle>EXPERIÊNCIA</ExperienceSectionTitle>
-          {experiences.map((experience, experienceIndex) => (
-            <ExperienceSectionInfo key={experienceIndex}>
-              <h2>{experience.title}</h2>
-              <h3>{experience.location}</h3>
-              <h4>{experience.period}</h4>
-              <p>{experience.description}</p>
+    <ExperienceContainer id="experiencia">
+      <ExperienceSection>
+        <ExperienceSectionTitle>EXPERIÊNCIA</ExperienceSectionTitle>
+        {experiences.map((experience, experienceIndex) => (
+          <ExperienceSectionInfo key={experienceIndex}>
+            <h2>{experience.title}</h2>
+            <h3>{experience.location}</h3>
+            <h4>{experience.period}</h4>
+            <p>{experience.description}</p>
+          </ExperienceSectionInfo>
+        ))}
+      </ExperienceSection>
+      <ExperienceSection>
+        <ExperienceSectionTitle>EDUCAÇÃO</ExperienceSectionTitle>
+        {educationalInstitutions.map(
+          (educationalInstitution, educationalInstitutionIndex) => (
+            <ExperienceSectionInfo key={educationalInstitutionIndex}>
+              <h2>{educationalInstitution.course}</h2>
+              <h3>{educationalInstitution.name}</h3>
+              <h4>{educationalInstitution.period}</h4>
             </ExperienceSectionInfo>
-          ))}
-        </ExperienceSection>
-        <ExperienceSection>
-          <ExperienceSectionTitle>EDUCAÇÃO</ExperienceSectionTitle>
-          {educationalInstitutions.map(
-            (educationalInstitution, educationalInstitutionIndex) => (
-              <ExperienceSectionInfo key={educationalInstitutionIndex}>
-                <h2>{educationalInstitution.course}</h2>
-                <h3>{educationalInstitution.name}</h3>
-                <h4>{educationalInstitution.period}</h4>
-              </ExperienceSectionInfo>
-            )
-          )}
-          <ExperienceSectionTitle className="stack">
-            TECNOLOGIAS
-          </ExperienceSectionTitle>
-          <ExperienceSectionStack>
-            <IconHtml withLabel={true} />
-            <IconCss withLabel={true} />
-            <IconJavaScript withLabel={true} />
-            <IconJquery withLabel={true} />
-            <IconReact withLabel={true} />
-            <IconAngular withLabel={true} />
-            <IconGit withLabel={true} />
-            <IconFigma withLabel={true} />
-            <IconPhp withLabel={true} />
-            <IconSql withLabel={true} />
-          </ExperienceSectionStack>
-        </ExperienceSection>
-      </ExperienceContainer>
-    </>
+          )
+        )}
+        <ExperienceSectionTitle className="stack">
+          TECNOLOGIAS
+        </ExperienceSectionTitle>
+        <ExperienceSectionStack>
+          <IconHtml withLabel={true} />
+          <IconCss withLabel={true} />
+          <IconJavaScript withLabel={true} />
+          <IconJquery withLabel={true} />
+          <IconReact withLabel={true} />
+          <IconAngular withLabel={true} />
+          <IconGit withLabel={true} />
+          <IconFigma withLabel={true} />
+          <IconPhp withLabel={true} />
+          <IconSql withLabel={true} />
+        </ExperienceSectionStack>
+      </ExperienceSection>
+    </ExperienceContainer>
   );
 }
